34b66f25bca25c514fa1e1f7aead42433f1a08e3
Ben Meier <benmeier@fastmail.com>
2016-09-08 20:45:35 +0200

    allowed mark-me-down to randomise port by default

--- markmedown.go
+++ markmedown.go
@@ -2,8 +2,10 @@ package main
 
 import (
     "os"
+    "errors"
     "flag"
     "fmt"
+    "net"
     "net/http"
     "io/ioutil"
     "os/signal"
@@ -23,7 +25,7 @@ The --listen-port field is provided in order to specify a particular port.
 `
 
 // address to listen on
-const listenAddress = "localhost"
+const listenAddress = "127.0.0.1"
 
 // html template for wrapping the markdown html into proper html
 const htmlTemplate = "<html><head><title>%s</title><style>%s</style></head><body class=\"markdown-body\">%s</body></html>"
@@ -41,9 +43,9 @@ func buildMarkdownFileServer(filepath string) func(http.ResponseWriter, *http.Re
     }
 }
 
-func main() {
+func mainInner() error {
     // first set up config flag options
-    listenPortFlag := flag.Int("listen-port", 80, "Server the markdown on this port")
+    listenPortFlag := flag.Int("listen-port", 0, "Server the markdown on this port")
 
     // set a more verbose usage message.
     flag.Usage = func() {
@@ -55,33 +57,38 @@ func main() {
 
     // expect a single argument
     if flag.NArg() != 1 {
-        os.Stderr.WriteString("A single input-file is required as argument 1. Use --help to see the usage.\n")
-        os.Exit(1)
+        return errors.New("A single input-file is required as argument 1. Use --help to see the usage")
     }
 
     // a listen port
-    if *listenPortFlag < 1 {
-        os.Stderr.WriteString("listen-port must be > 0. Use --help to see the usage.\n")
-        os.Exit(1)
+    if *listenPortFlag < 0 {
+        return errors.New("listen-port must be >= 0. Use --help to see the usage")
     }
 
     // register http function for handling argument /
     http.HandleFunc("/", buildMarkdownFileServer(flag.Args()[0]))
 
+    // set up server object
+    server := &http.Server{Addr: fmt.Sprintf("%s:%d", listenAddress, *listenPortFlag), Handler: nil}
+    ln, err := net.Listen("tcp", server.Addr)
+    if err != nil { return err }
+
+    // cast to TCPListener
+    tp := ln.(*net.TCPListener)
+    fmt.Printf("Listening on %s...\n", tp.Addr())
+
     // begin serving
     go func() {
-        err := http.ListenAndServe(fmt.Sprintf("%s:%d", listenAddress, *listenPortFlag), nil)
+        err := server.Serve(ln)
         if err != nil {
-            os.Stderr.WriteString(err.Error() + "\n")
+            os.Stderr.WriteString(fmt.Sprintf("%s\n", err.Error()))
             os.Exit(1)
         }
     }()
 
-    fmt.Printf("Listening on %s:%d...\n", listenAddress, *listenPortFlag)
-
     // open the server in the available browser
     fmt.Println("Attempting to open a browser window to the address..")
-    browser.OpenURL(fmt.Sprintf("http://%s:%d", listenAddress, *listenPortFlag))
+    browser.OpenURL(fmt.Sprintf("http://%s", tp.Addr()))
 
     // instead of sitting in a for loop or something, we wait for sigint
     signalChannel := make(chan os.Signal, 1)
@@ -89,6 +96,16 @@ func main() {
     signal.Notify(signalChannel, os.Interrupt)
     for sig := range signalChannel {
         fmt.Printf("Received %v signal. Stopping.\n", sig)
-        os.Exit(0)
+        return nil
+    }
+    return nil
+}
+
+
+func main() {
+    err := mainInner()
+    if err != nil {
+        os.Stderr.WriteString(fmt.Sprintf("%s\n", err.Error()))
+        os.Exit(1)
     }
 }
